f35e3220994054856b7ef37a15c743bad1213b9d
DerrickDarku <derrickalberto@ymail.com>
2021-10-05 15:45:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>MEL ARTHUR PHOTOGRAPHY</h1>
+       <a href="">PROFILE</a>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
